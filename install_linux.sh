153df275aacb042f467dc4e12c0ecf0be4f208fd
Ty Overby <ty@pre-alpha.com>
2025-10-04 16:36:37 +0100

    _

---
+++ install_linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euox pipefail
+
+sudo apt install bubblewrap curl autoconf build-essential
+
+curl -fsSL https://opam.ocaml.org/install.sh > install_opam.sh
+chmod +x install_opam.sh
+./install_opam.sh --download-only
+mv opam-* ./opam
+chmod +x opam
+
+./opam init --disable-sandboxing
+./opam update --all
+./opam switch create 5.2.0+ox --repos ox=git+https://github.com/oxcaml/opam-repository.git,default
+eval $(./opam env --switch 5.2.0+ox)
+./opam install -y ocamlformat merlin ocaml-lsp-server
